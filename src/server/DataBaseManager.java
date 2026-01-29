ee979e9c31d8232d8bbfdae4ca419ab054ec5e98
dem030 <demetrio.priolo2003@gmail.com>
2026-01-29 17:20:30 +0100

    add server e db

--- src/server/DataBaseManager.java
+++ src/server/DataBaseManager.java
@@ -1,5 +1,33 @@
 package server;
+import java.sql.*;
 
 public class DataBaseManager {
+    static private DataBaseManager istance = null;
+    private Connection conn;
+    private String dbUrl;
+    private String dbUsername;
+    private String dbPassword;
 
+    private DataBaseManager() {
+    }
+    static getIstance() {
+        if (istance == null) {
+            istance = new DataBaseManager();
+        }
+        return istance;
+    }
+    public void inizializza(String dburl, String username, String password){
+        try {
+            this.dbUrl= dburl;
+            this.dbUsername= username;
+            this.dbPassword= password;
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(dburl, username, password);
+
+        } catch (Exception e) {
+            
+        }
+
+    }
+    
 }
--- src/server/ServerMain.java
+++ src/server/ServerMain.java
@@ -1,5 +1,59 @@
 package server;
+import utils.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
 
 public class ServerMain {
+    private int porta;
+    private int maxThread;
+    ExecutorService threadPool;
+    ServerSocket serverSocket;
+    Configurazione config;
+    DataBaseManager dbManager;
+    boolean inEsecuzione;
+    public void chiudiServer() {
+        inEsecuzione = false;
+        if (threadPool != null) {
+            threadPool.shutdown();
+            try{
+                if (! threadPool.awaitTermination(10, java.util.concurrent.TimeUnit.SECONDS)) {
+                    threadPool.shutdownNow();
 
+                }
+            } catch (InterruptedException e) {
+                threadPool.shutdownNow();
+            }
+            if (serverSocket != null && !serverSocket.isClosed()) {
+                serverSocket.close();
+            }
+            if (dbManager != null) {
+                dbManager.closeConnection();
+                
+            }
+        }
+    }
+
+    public void main(String[] args) {
+        try {
+            XMLConfigParser.leggiConfigurazione("../../config/config.xml");
+            dbManager = DataBaseManager.getIstance();
+            dbManager.inizializza(config.getDbUrl(), config.getDbUsername(), config.getDbPassword());
+            threadPool = Executor.newFixedThreadPool(maxThread);
+            serverSocket = new ServerSocket(config.getPortaServer());
+            inEsecuzione = true;
+            while (inEsecuzione) {
+                Socket clientSocket = serverSocket.accept();
+                CLientHandler clientHandler = new ClientHandler(clientSocket, config);
+                threadPool.submit(clientHandler);
+
+            }
+        } catch (Exception e) {
+            if (inEsecuzione) {
+                System.err.println("Errore nel server: " + e.getMessage());
+            }
+        }
+        finally { chiudiServer(); }
+    }
 }
